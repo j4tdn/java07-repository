5a0e2dba59a3fea851f72711170d6accb760a461
lenhatphi <philenhat123aaa@gmail.com>
2020-04-05 23:10:55 +0700

    Lesson09-LeNhatPhi submit code

---
+++ lesson09-Sumit-LeNhatPhi/src/test/java/ava07/lesson09_Sumit_LeNhatPhi/AppTest.java
@@ -0,0 +1,20 @@
+package ava07.lesson09_Sumit_LeNhatPhi;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
