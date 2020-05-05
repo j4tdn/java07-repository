57c9552941ee31b7ac52cdc823317605a2967445
lenhatphi <philenhat123aaa@gmail.com>
2020-05-05 16:05:28 +0700

    Lesson08-LeNhatPhi submit code 1

--- lesson08_LeNhatPhi/src/exercise/Ex07.java
+++ lesson08_LeNhatPhi/src/exercise/Ex07.java
@@ -2,6 +2,14 @@ package exercise;
 
 public class Ex07 {
 	public static void main(String[] args) {
+<<<<<<< HEAD
+=======
+		maxIndex();
+	
+	}
+
+	private static void maxIndex() {
+>>>>>>> 8b1524c... Lession08-LeNhatPhi submit code 1
 		String string = "aaabaaabbaaaaa";
 		int current = 0;
 		int count = 1;
@@ -23,7 +31,11 @@ public class Ex07 {
 			max = count;
 			maxIndex = current;
 		}
+<<<<<<< HEAD
 		System.out.println("Vị trí thứ: "+"["+maxIndex+"]");
+=======
+		System.out.println("Vị trí thứ: " + "[" + maxIndex + "]");
+>>>>>>> 8b1524c... Lession08-LeNhatPhi submit code 1
 		System.out.print("Chuỗi dài nhất:");
 		for (int i = maxIndex; i < maxIndex + max; i++) {
 			System.out.print(string.charAt(i));
