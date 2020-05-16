f4576d5b2a2760088bfc42f5dba6b3670901cf8d
Quyen Phan <Quyen.Phan@mgm-tp.com>
2020-05-17 00:51:41 +0700

    Lesson10 - 14.05.2010

---
+++ Lesson10-PhanNgocQuyen/src/main/java/beans/Student.java
@@ -0,0 +1,64 @@
+package beans;
+
+public class Student implements Comparable<Student>{
+	private String id;
+	private String fullname;
+	private double avg;
+	private boolean gender;
+
+	public Student() {
+	}
+
+	public Student(String id, String fullname, double avg, boolean gender) {
+		this.id = id;
+		this.fullname = fullname;
+		this.avg = avg;
+		this.gender = gender;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getFullname() {
+		return fullname;
+	}
+
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+
+	public double getAvg() {
+		return avg;
+	}
+
+	public void setAvg(double avg) {
+		this.avg = avg;
+	}
+
+	public boolean isGender() {
+		return gender;
+	}
+
+	public void setGender(boolean gender) {
+		this.gender = gender;
+	}
+
+	@Override
+	public String toString() {
+		return id + ", " + fullname + ", " + avg + ", " + gender;
+	}
+
+	@Override
+	public int compareTo(Student o) {
+		System.out.println("----------AAA------------");
+		return Boolean.compare(o.isGender(), isGender());
+	}
+
+	
+	
+}
