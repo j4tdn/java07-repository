3d83130f4217f39df847c0a57443722e0001466a
phanbinh <phanbinh2671999@gmail.com>
2020-05-20 00:08:33 +0700

    lesson10-ex-20.5.2020

--- Lesson10-Exercise/src/main/java/ex1/Ex01.java
+++ Lesson10-Exercise/src/main/java/ex1/Ex01.java
@@ -5,10 +5,10 @@ import java.util.Arrays;
 public class Ex01 {
 	public static void main(String[] args) {
 		int[] digits = {0, 1, 2, 4, 3, 1, 3, 0, 0, 0 };
-		Arrays.stream(getUniqueNumbers(digits)).forEach(digit -> System.out.println(digit + " "));
+		Arrays.stream(distinct(digits)).forEach(digit -> System.out.println(digit + " "));
 	}
 
-	private static int[] getUniqueNumbers(int[] digits) {
+	public static int[] getUniqueNumbers(int[] digits) {
 		int[] result = new int[digits.length];
 		int index = 0;
 		for (int i = 0; i < digits.length; i++) {
@@ -19,8 +19,20 @@ public class Ex01 {
 
 		return Arrays.copyOfRange(result, 0, index);
 	}
+	
+	public static int[] distinct(int[] digits) {
+		int[] result = new int[digits.length];
+		int index = 0;
+		for (int i = 0; i < digits.length; i++) {
+			if (contains(result, digits[i], index)) {
+				result[index++] = digits[i];
+			}
+		}
 
-	private static boolean contains(int[] digits, int number, int index) {
+		return Arrays.copyOfRange(result, 0, index);
+	}
+
+	public static boolean contains(int[] digits, int number, int index) {
 		for (int i = 0; i < digits.length; i++) {
 			if (i !=index && number == digits[i]) {
 				return false;
--- Lesson10-Exercise/src/main/java/ex1/Ex04.java
+++ Lesson10-Exercise/src/main/java/ex1/Ex04.java
@@ -1,28 +1,12 @@
 package ex1;
 
+import java.util.Arrays;
+
 public class Ex04 {
 	public static void main(String[] args) {
-		int[] numbers = {1, 2, 5, 8, 9, 10, 5, 8, 9, 12, 7, 7, 6};
-		System.out.println(thirdMax(numbers));;
-	}
-	
-	private static int max(int[] numbers) {
-		int max = numbers[0];
-		for (int number : numbers) {
-			if (max < number) {
-				max = number;
-			}
-		}
-		return max;
-	}
-	
-	private static int secondMax(int[] numbers) {
-		int thirdMax = numbers[0];
-		for (int number : numbers) 	{
-			if (number != max(numbers) && number != thirdMax(numbers) && thirdMax < number) {
-				thirdMax = number;
-			}
-		}
-		return thirdMax;
+		int[] numbers = {1, 3, 5, 8, 4, 5, 4, 2, 1};
+		Arrays.sort(numbers);
+		numbers = Ex01.distinct(numbers);
+		System.out.println(numbers[numbers.length - 3]);
 	}
 }
