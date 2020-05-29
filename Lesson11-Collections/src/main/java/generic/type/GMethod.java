a745417783903e50096c2c292796dc86e1e666a7
longho <hoduylong411@gmail.com>
2020-05-29 09:56:22 +0700

    Lesson11 - HoDuyLong - 28.05.2020

---
+++ Lesson11-Collections/src/main/java/generic/type/GMethod.java
@@ -0,0 +1,57 @@
+package generic.type;
+
+public class GMethod {
+	public static void main(String[] args) {
+		String s = "string";
+		Integer i = 20;
+		Float f = 30f;
+
+		printO(s);
+		printO(i);
+		printO(f);
+		System.out.println("====================");
+
+		printG(s);
+		printG(i);
+		printG(f);
+		System.out.println("====================");
+
+		String[] strings = { "1", "a", "b", "c", "d" };
+		Integer[] integers = { 1, 2, 3, 4, 5 };
+		Double[] doubles = { 1d, 2d, 3d, 4d, 5d };
+		Object[] objects = { "a", 2, "c", 4, "e" };
+
+		show(strings);
+		show(integers);
+		show(doubles);
+		show(objects);
+		System.out.println("====================");
+
+		showG(objects);
+		showG(integers);
+		showG(doubles);
+		showG(objects);
+	}
+
+	private static void printO(Object o) {
+		System.out.println(o);
+	}
+
+	private static <G> void printG(G g) {
+		System.out.println(g);
+	}
+
+	private static void show(Object[] objects) {
+		for (Object object : objects) {
+			System.out.print(object + " ");
+		}
+		System.out.println();
+	}
+
+	private static <E> void showG(E[] elements) {
+		for (Object element : elements) {
+			System.out.print(element + " ");
+		}
+		System.out.println();
+	}
+}
---
+++ Lesson11-Collections/src/main/java/generic/type/GMethodEx01.java
@@ -0,0 +1,26 @@
+package generic.type;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class GMethodEx01 {
+	public static void main(String[] args) {
+		List<Integer> integers = Arrays.asList(1, 2, 3, 4, 5);
+		List<String> strings = Arrays.asList("1", "2", "3", "4", "5");
+		List<Double> doubles = Arrays.asList(1d, 2d, 3d, 4d, 5d);
+		List<Object> objects = Arrays.asList(1d, "b", 3d, "d", 5d);
+
+		// print(objects);
+		print(integers);
+		// print(strings);
+		print(doubles);
+	}
+
+	// bound type: Number
+	private static <E extends Number> void print(List<E> elements) {
+		for (E element : elements) {
+			System.out.print(element + " ");
+		}
+		System.out.println();
+	}
+}
