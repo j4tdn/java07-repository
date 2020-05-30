40b426ac7d81d26c481acf423d4716f4cd761d58
khanhga10b1 <hokhanha2@gmail.com>
2020-05-30 12:14:00 +0700

    Lesson11-30.05.2020

--- Lesson11-Collections/src/main/java/generic/type/FakeList.java
+++ Lesson11-Collections/src/main/java/generic/type/FakeList.java
@@ -1,6 +1,7 @@
 package generic.type;
 
 import java.lang.reflect.Array;
+import java.util.function.Consumer;
 
 public class FakeList<E> {
 	private E[] es;
@@ -8,6 +9,58 @@ public class FakeList<E> {
 	@SuppressWarnings("unchecked")
 	public FakeList() {
 		es = (E[]) Array.newInstance(Object.class, 0);
+	}
+	
+	public boolean remove(E e) {
+		 for(int i =0;i<es.length;i++) {
+			 if(es[i].equals(e)) {
+				 remove(i);
+			 }
+		 }
+		return true;
+	}
+
+	public boolean remove(int index) {
+		if(index<0 ||index> es.length) {
+			System.out.println("xoa khong thanh cong");
+			return false;
+		}
+		@SuppressWarnings("unchecked")
+		E[] newEs = (E[]) Array.newInstance(Object.class, es.length - 1);
+		for (int i = 0; i < newEs.length; i++) {
+			if (i < index) {
+				newEs[i] = es[i];
+			} else  {
+				newEs[i]=es[i+1];
+			}
+		}
+		es = newEs;
+		return true;
+	}
+
+	public void forEach(Consumer<E> action) {
+		for(int i =0;i<es.length;i++) {
+			action.accept(es[i]);
+		}
+	}
+	public boolean add(E e, int index) {
+		if(index<0 ||index> es.length) {
+			System.out.println("add khong thanh cong");
+			return false;
+		}
+		@SuppressWarnings("unchecked")
+		E[] newEs = (E[]) Array.newInstance(Object.class, es.length + 1);
+		for (int i = 0; i < newEs.length; i++) {
+			if (i < index) {
+				newEs[i] = es[i];
+			} else if (i == index) {
+				newEs[i] = e;
+			} else {
+				newEs[i] = es[i - 1];
+			}
+		}
+		es = newEs;
+		return true;
 	}
 
 	public boolean add(E e) {
--- Lesson11-Collections/src/main/java/generic/type/GTypeDemo.java
+++ Lesson11-Collections/src/main/java/generic/type/GTypeDemo.java
@@ -7,9 +7,19 @@ public class GTypeDemo {
 		strings.add("Fake Item 01");
 		strings.add("Fake Item 02");
 		strings.add("Fake Item 03");
-		
-		
+		strings.add("a", 2);
+		strings.add("a");
+		strings.forEach(string->System.out.print(string+" "));
+		System.out.println("\n==========");
+		strings.remove("a");
+		strings.forEach(string->System.out.print(string+" "));
+		System.out.println("\n=============");
 		System.out.println("size: "+strings.size());
 		System.out.println(strings.getClass());
+		
+		// forEach
+		// add(int index, E e)
+		// remove(int i)
+		// remove(E e)
 	}
 }
