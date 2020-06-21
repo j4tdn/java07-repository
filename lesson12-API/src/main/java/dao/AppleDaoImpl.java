8a59b60b50e3116905ba78403ac4a77a0acbb160
lenhatphi <philenhat123aaa@gmail.com>
2020-06-21 11:07:23 +0700

    Lesion12-API-LeNhatPhi-21/06/20

--- lesson12-API/src/main/java/dao/AppleDaoImpl.java
+++ lesson12-API/src/main/java/dao/AppleDaoImpl.java
@@ -15,16 +15,18 @@ public class AppleDaoImpl implements AppleDao {
 	
 	private List<Apple> mockData(){
 		
-//		Store s0 = new Store("S0","Store100","A0");
-//		Store s1 = new Store("S1","Store101","A1");
-//		Store s2 = new Store("S2","Store102","A2");
-//		Store s3 = new Store("S3","Store103","A2");
-//		
+		//		Store s0 = new Store("S0","Store100","A0");
+		//		Store s1 = new Store("S1","Store101","A1");
+		//		Store s2 = new Store("S2","Store102","A2");
+		//		Store s3 = new Store("S3","Store103","A2");
 		List<Apple> apple = new ArrayList<>();
 		apple.add(new Apple("green","Lao", 80));
 		apple.add(new Apple("red","Lao", 160));
 		apple.add(new Apple("green","VN", 120));
+		//duplicate apple 
+		apple.add(new Apple("green","Lao", 80));
 		apple.add(new Apple("red","VN", 180));
+		
 		return apple;
 	}
 }
--- lesson12-API/src/main/java/stream/Ex01StreamDemo.java
+++ lesson12-API/src/main/java/stream/Ex01StreamDemo.java
@@ -1,7 +1,16 @@
 package stream;
 
+import java.io.ObjectInputStream.GetField;
+import java.lang.reflect.Array;
+import java.security.KeyStore.Entry;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -14,11 +23,31 @@ public class Ex01StreamDemo {
 
 	public static void main(String[] args) {
 		List<Apple> inventory = service.getAll();
-		Set<String> result = inventory.stream().filter(a -> a.getWeight() > 150)
-				.limit(4)
-				.map(Apple::getOrigin)
-				.collect(Collectors.toSet());
+
+		// external interation for theo ý của mình
+
+		// internal interation || loop
+		List<String> result = inventory.stream().filter(a -> a.getWeight() > 150).limit(4).map(Apple::getOrigin)
+				.distinct().collect(Collectors.toList());
 		result.forEach(System.out::println);
 		System.out.println("================================");
+
+		// distinct theo 1 thuộc tính thì map(key,value)
+
+		Map<String, Apple> map = inventory.stream()
+				.collect(Collectors.toMap(Apple::getOrigin, a -> a, (a1,a2) -> a1,LinkedHashMap :: new ));
+		
+		List<Apple> distinct = map.entrySet().stream()
+				.map(java.util.Map.Entry::getValue)
+				.collect(Collectors.toList());
+		distinct.forEach(System.out::println);
+		System.out.println("===============================");
+		Set<String> originSet = new HashSet<>();
+		Array[] apples = inventory.stream().filter(disticByKey(Apple::getOrigin)).toArray(Apple);
+		Arrays.stream(apples).forEach(System.out::println);
+	}
+	private static<T,R> Predicate<T> disticByKey(Function<T, R> func){
+		Set<String> origins = new HashSet<String>();
+		return a -> origins.add((String) func.apply(a));
 	}
 }
