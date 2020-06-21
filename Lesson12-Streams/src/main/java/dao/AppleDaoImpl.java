2f1254daf32f0e27d787ff6a5a47a23e36ca12c3
Dang Hoang <danghoanght98@gmail.com>
2020-06-21 11:16:03 +0700

    Lesson12 21-06-2020

--- Lesson12-Streams/src/main/java/dao/AppleDaoImpl.java
+++ Lesson12-Streams/src/main/java/dao/AppleDaoImpl.java
@@ -17,9 +17,10 @@ public class AppleDaoImpl implements AppleDao {
 		Store s2 = new Store("s2", "Store 2", "A102");
 		List<Apple> apples = new ArrayList<>();
 		apples.add(new Apple("green", "VN", 120, s1));
-		apples.add(new Apple("green", "VN", 150, s1));
+		apples.add(new Apple("green", "JP", 150, s1));
 		apples.add(new Apple("red", "VN", 160, s2));
 		apples.add(new Apple("green", "VN", 100, s2));
+		apples.add(new Apple("green", "TL", 120, s1));
 		apples.add(new Apple("red", "Lao", 150, s1));
 		return apples;
 	}
