4999907ef9a1444b7f92069db99b222c715df26a
phanbinh <phanbinh2671999@gmail.com>
2020-07-02 22:50:32 +0700

    Lesson14 - PhanThanhBinh 02.07.2020

--- Lesson14-PhanThanhBinh/src/main/java/beans/Student.java
+++ Lesson14-PhanThanhBinh/src/main/java/beans/Student.java
@@ -5,31 +5,17 @@ import java.util.Objects;
 public class Student {
 	private String id;
 	private String name;
+	private Float score;
 	private String gender;
 	public Student() {
 		super();
 	}
-	public Student(String id, String name, String gender) {
+	public Student(String id, String name, Float score, String gender) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.score = score;
 		this.gender = gender;
-	}
-	
-	@Override
-	public int hashCode() {
-		return Objects.hash(getId());
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Student other = (Student) obj;
-		return other.getId().equals(getId()) && other.getName().equals(getName()) && other.getGender().equals(getGender());
 	}
 	public String getId() {
 		return id;
@@ -43,6 +29,12 @@ public class Student {
 	public void setName(String name) {
 		this.name = name;
 	}
+	public Float getScore() {
+		return score;
+	}
+	public void setScore(Float score) {
+		this.score = score;
+	}
 	public String getGender() {
 		return gender;
 	}
@@ -51,7 +43,49 @@ public class Student {
 	}
 	@Override
 	public String toString() {
-		return id + ", " + name + ", " + gender;
+		return "Student [id=" + id + ", name=" + name + ", score=" + score + ", gender=" + gender + "]";
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((gender == null) ? 0 : gender.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((score == null) ? 0 : score.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		if (gender == null) {
+			if (other.gender != null)
+				return false;
+		} else if (!gender.equals(other.gender))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (score == null) {
+			if (other.score != null)
+				return false;
+		} else if (!score.equals(other.score))
+			return false;
+		return true;
+	}
+	
 	
 }
--- Lesson14-PhanThanhBinh/src/main/java/test/Ex04.java
+++ Lesson14-PhanThanhBinh/src/main/java/test/Ex04.java
@@ -15,8 +15,10 @@ import beans.Student;
 public class Ex04 {
 	public static void main(String[] args) throws IOException {
 		List<Student> students = getAll("student.txt");
-		
+		List<Student> preScore = getPredicate(students, a -> a.getScore() > 8);
 		List<Student> preGender = getPredicate(students, a -> a.getGender().equals("Nu"));
+		List<Student> sortGender = sort(students, Comparator.comparing(Student::getGender).thenComparing(Student::getScore).reversed());
+		
 	}
 	
 	private static List<Student> getAll(String pathname) throws IOException {
@@ -28,7 +30,7 @@ public class Ex04 {
 					@Override
 					public Student apply(String t) {
 						String[] attributes = t.split(", ");
-						Student student = new Student(attributes[0], attributes[1], attributes[2]);
+						Student student = new Student(attributes[0], attributes[1], Float.parseFloat(attributes[2]), attributes[3]);
 						return student;
 					}
 				}).collect(Collectors.toList());
@@ -42,4 +44,11 @@ public class Ex04 {
 		return rs;
 	}
 	
+	private static List<Student> sort(List<Student> students, Comparator<Student> comparator) {
+		List<Student> rs = new ArrayList<>();
+		rs = students.stream()
+				.sorted(comparator)
+				.collect(Collectors.toList());
+		return rs;
+	}
 }
