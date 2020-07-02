a24ef2190c44b6e5391c46ec37b9ba1f5a7b632d
phanbinh <phanbinh2671999@gmail.com>
2020-07-02 20:58:56 +0700

    Lesson13 - 2.7.2020

--- Lesson13-Files/src/main/java/content/Ex01Readble.java
+++ Lesson13-Files/src/main/java/content/Ex01Readble.java
@@ -1,29 +1,40 @@
 package content;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import java.awt.Desktop;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.Collections;
 
 import beans.Customer;
 import utils.FileUtils;
 
-public class Ex01 {
+public class Ex01Readble {
 	public static void main(String[] args) throws IOException {
 		List<Customer> customers = getAll();
 		String pathname = "JAVA07" +File.separator + "data" + File.separator + "customer.txt";
 		File file = FileUtils.createFile(pathname);
-		FileUtils.writeFile(file, customers);
+	//	FileUtils.writeFile(file, customers);
+		List<String> lines = customers.stream().map(Customer::toString).collect(Collectors.toList());
+		Files.write(file.toPath(), lines);
+		
+		//readfile
+		System.out.println("Readfile:");
+		List<String> result = Files.readAllLines(file.toPath());
+		result.forEach(System.out::println);
 		
 		//open file
 		Desktop.getDesktop().open(file);
 	}
 	
-	private static List<Customer> getAll() {
+	public static List<Customer> getAll() {
 		return Arrays.asList(
-				new Customer("Le Na", "0333233518", "Da Nang"),
+				new Customer("Le Na", "0333233518", "Sai gon"),
 				new Customer("Hoang Teo", "0934730381", "Quang Nam"),
 				new Customer("Thanh Binh", "0362551688", "Hue"));
 	}
