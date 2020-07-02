6f79b17f67f215294426cdd4b06a7a1d4bd0f931
hokhanh <hokhanha2@gmail.com>
2020-07-02 07:26:01 +0700

    add

--- Lesson13-Files/src/main/java/structure/Ex03.java
+++ Lesson13-Files/src/main/java/structure/Ex03.java
@@ -10,8 +10,8 @@ import ultils.FileUltils;
 public class Ex03 {
 	public static void main(String[] args) throws IOException {
 		// create 20 files with extension(mp3,mp4,jpg)
-		File parent = new File("JAVA07");
-//		FileUltils.createFiles(parent, 20, Extension.values());
+		File parent = new File("JAVA09");
+		FileUltils.createFiles(parent, 20, Extension.values());
 		// delete file with java extension
 		File[] files = parent.listFiles(file->file.getName().endsWith("."+Extension.JAVA.getValue()));
 		System.out.println("length: " + files.length);
