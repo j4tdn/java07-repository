b97a5d7f07b3040dd7572c052504686ecfdcfe73
longho <hoduylong411@gmail.com>
2020-10-22 22:43:11 +0700

    Connection to MySQL

---
+++ Lesson17-DBConnection/src/main/java/connection/JdbcConnection.java
@@ -0,0 +1,32 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JdbcConnection {
+
+	private static final String URL = "jdbc:mysql://localhost:3306/java07_shopping";
+	private static final String USER = "root";
+	private static final String PASSWORD = "hoduylong2709";
+
+	public static Connection getMySQL() {
+		Connection connection = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = DriverManager.getConnection(URL, USER, PASSWORD);
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (connection != null) {
+					connection.close();
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return connection;
+	}
+}
